e4adddffc40df90000d6a190ce03920a5e548ff7
Parisa <134794618+parisa-0@users.noreply.github.com>
2023-12-08 11:28:12 +0000

    Create GitTraining.java

---
+++ GitTraining.java
@@ -0,0 +1,24 @@
+/*
+git config -- list
+git config user.name
+
+man git
+git help config
+
+Initialize git repo
+cd desktop/{name of folder}
+git init
+cd .git
+ls
+
+Write to your READ.me
+echo "# write to readme" >> README.md
+Add to staging area
+git add .
+Commit w/message
+git commit -m "first commit"
+Add origin git repo to remote repo
+git remote add origin
+git push -u origin master
+
+*/

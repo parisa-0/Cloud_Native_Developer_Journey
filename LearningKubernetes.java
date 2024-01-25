3ed9bae03b9af32d61d575f71d5531e17eb9a725
Parisa <134794618+parisa-0@users.noreply.github.com>
2024-01-25 22:19:40 +0000

    Create LearningKubernetes.java

---
+++ LearningKubernetes.java
@@ -0,0 +1,11 @@
+/*
+Kubernetes is a system to help run containers on Linux and Windows servers, with the underlying servers also being managed by the K8s control panel
+A node is a system which can be managed by Kubernetes, that can run containerized workloads
+The control plane runs the API server, scheduler and controller manager, and works with Etcd to understand and provide access to the rest of the system
+Distributed system.
+Container images and Trivy scanning tool to understadn potential issues with cluster
+
+minikube.sigs.k8s.io/docs/start
+
+https://docs.cilium.io/en/latest/security/policy/kubernetes/#namespaces
+*/
